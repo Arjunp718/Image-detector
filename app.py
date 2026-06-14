d579c64e33c03aff5e070e427b6bd5f34d9799f4
Arjunp718 <arjunprushothaman@gmail.com>
2026-06-14 14:21:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,33 @@
 import streamlit as st
+from PIL import Image
+from PIL.ExifTags import TAGS, GPSTAGS
 
-st.title("Image Origin Detector")
+st.title("Photo Location Detector")
 
 uploaded_file = st.file_uploader(
     "Upload an image",
     type=["jpg", "jpeg", "png"]
 )
 
+def get_exif_data(image):
+    exif = {}
+    info = image._getexif()
+
+    if info:
+        for tag, value in info.items():
+            decoded = TAGS.get(tag, tag)
+            exif[decoded] = value
+
+    return exif
+
 if uploaded_file:
-    st.image(uploaded_file)
+    image = Image.open(uploaded_file)
+    st.image(image)
 
-    st.success("Image uploaded!")
+    exif = get_exif_data(image)
 
-    st.write("Analysis will go here.")
+    if "GPSInfo" in exif:
+        st.success("GPS data found!")
+        st.write(exif["GPSInfo"])
+    else:
+        st.error("No GPS location found in this image.")
